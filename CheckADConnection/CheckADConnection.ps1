ba55a3b35b1de4c1029b8de2a8089d9f4a6f068d
DehnCrest <dehncrest.yt@gmail.com>
2021-10-28 12:53:14 +0200

    Update CheckADConnection.ps1

--- CheckADConnection/CheckADConnection.ps1
+++ CheckADConnection/CheckADConnection.ps1
@@ -1,7 +1,15 @@
 $path = (Get-Item -Path "./").FullName
 $servers = Get-Content "$path\servers.txt"
-$Credentials = Get-Credential -Message 'Please enter an Administrator account'
+$RSATTools = Get-WindowsCapability -Name Rsat.Active* -Online
+if ($RSATTools.State -eq 'NotPresent') {
+    Write-Host "RSAT Tools not found, installing the package" -ForegroundColor Yellow
+    Add-WindowsCapability -Name $RSATTools.Name -Online
+}
+else {
+    Write-Host 'RSAT Tools is already installed, skipping installation' -ForegroundColor Yellow
+}
 
+$AdminSESA = Get-Credential -Message 'Please enter an ADM account'
 foreach ($server in $servers) {
     $obj = New-Object PSObject
     $obj | Add-Member -MemberType NoteProperty -Name "ServerName" -Value $server
@@ -10,7 +18,7 @@ foreach ($server in $servers) {
         
     }
     try {
-        $res = Get-ADComputer -Identity $server -Credential $Credentials -ErrorAction Stop
+        $res = Get-ADComputer -Identity $server -Credential $AdminSESA -ErrorAction Stop
         Write-Host $res -ForegroundColor Green
         $obj | Add-Member -MemberType NoteProperty -Name "ADStatus" -Value "Found in AD"
         $obj | Add-Member -MemberType NoteProperty -Name "FullInfo" -Value $res
